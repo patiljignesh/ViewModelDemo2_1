52698f7fa83a141bb70e866c9529c97e37e5bb45
Jignesh Patil <patiljignesh@gmail.com>
2022-08-14 21:31:25 +0200

    Notes Add.

--- app/src/main/java/com/anushka/viewmodeldemo2/MainActivityViewModelFactory.kt
+++ app/src/main/java/com/anushka/viewmodeldemo2/MainActivityViewModelFactory.kt
@@ -6,6 +6,7 @@ import java.lang.IllegalArgumentException
 
 class MainActivityViewModelFactory(private val startingTotal: Int): ViewModelProvider.Factory {
 
+    //ViewModelFactory is for Custom ViewModels
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if(modelClass.isAssignableFrom(MainActivityViewModel::class.java)){
             return MainActivityViewModel(startingTotal) as T
